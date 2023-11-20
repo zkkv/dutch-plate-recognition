f876c96cd58d691161c957bace5a040c3c04adc8
Ana Băltărețu <a.baltaretu@student.tudelft.nl>
2023-11-20 17:34:49 +0100

    Try running main in build and change file path

--- main.py
+++ main.py
@@ -8,14 +8,16 @@ import numpy
 # for more information of 'argparse' module, see https://docs.python.org/3/library/argparse.html
 def get_args():
 	parser = argparse.ArgumentParser()
-	parser.add_argument('--file_path', type=str, default='TrainingVideo.avi')
+	parser.add_argument('--file_path', type=str, default='dataset/trainingvideo.avi')
 	parser.add_argument('--output_path', type=str, default=None)
 	parser.add_argument('--sample_frequency', type=int, default=2)
 	args = parser.parse_args()
 	return args
 
+
 # In this file, you need to pass three arguments into CaptureFrame_Process function.
 if __name__ == '__main__':
+	print("Hello from main!")
 	args = get_args()
 	if args.output_path is None:
 		output_path = os.getcwd()
